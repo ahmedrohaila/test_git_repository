06629d5f50b8bb9da570d423bbd64282d8378ce6
ahmedrohaila <inocentahmedhussain@gmail.com>
2023-09-26 21:47:01 +0500

    corrected the spelling of yes

--- ahmed.py
+++ ahmed.py
@@ -1,6 +1,6 @@
 x = input ("enter your name")
 if x == 'ahmed'
-	print("yws boss! what can i do for you?")
+	print("yes boss! what can i do for you?") // yes was mispelled in the previous code
 else 
 	print("get out of my private property!")
 
